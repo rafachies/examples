bf755ae2e401356450a8e7fc7d0b22b107b6a51a
Rafael Chies <rafachies@gmail.com>
2016-01-21 12:03:00 -0200

    Adding real log to the service implementation

--- dosgi-example/dosgi-services/src/main/java/br/com/redhat/dosgi/CalculatorServiceBean.java
+++ dosgi-example/dosgi-services/src/main/java/br/com/redhat/dosgi/CalculatorServiceBean.java
@@ -1,10 +1,13 @@
 package br.com.redhat.dosgi;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CalculatorServiceBean implements CalculatorService {
 
+	private Logger logger = LoggerFactory.getLogger(CalculatorServiceBean.class);
+	
 	public Integer sum(Integer x, Integer y) {
-		System.out.println("Hey, I was called by another container. DOSGI works!");
+		logger.info("Hey, I was called by another container. DOSGI works!");
 		return x + y;
 	}
 
